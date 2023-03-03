efac3234d44fc5dc9f8977532880e97e8e4850fa
PieroDiazCuri <pierodiazcuri@gmail.com>
2023-03-03 11:53:34 -0500

    feac:feature/nest-throttler

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Prueba de concepto nestjs/throttler';
+    return 'Prueba de concepto de nestjs/throttler';
   }
 }
